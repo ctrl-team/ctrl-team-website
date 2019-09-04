cb37dc27b465ba2ae6a12e75892d756efb268cc6
JaZa <47424360+JaZax@users.noreply.github.com>
2019-09-04 18:43:02 +0200

    added back to top button

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const logo_section = document.getElementById('logo');
+const backtotop = document.getElementById('backtotop');
+
+let logo_height = logo_section.offsetHeight;
+
+console.log(logo_height)
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > logo_height){
+        backtotop.style.display = 'block';
+        backtotop.style.opacity = 1;
+    }else{
+        backtotop.style.display = 'none'
+        backtotop.style.opacity = 0;
+    }
+})
+
+backtotop.addEventListener('click', ()=>{
+    window.scrollTo(0, 0)
+})
