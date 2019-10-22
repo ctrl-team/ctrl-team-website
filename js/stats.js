ed50264204115f8ad65071f2b8cbe7e381000201
JaZa <47424360+JaZax@users.noreply.github.com>
2019-10-22 17:12:05 +0200

    fixed commit messages bug

--- js/stats.js
+++ js/stats.js
@@ -87,12 +87,15 @@ req2.onreadystatechange = function () {
 
                 //LAST COMMIT
 
-                    fetch('https://api.github.com/repos/ctrl-team/' + repo.name + '/commits')
-                    .then(resp => resp.json())
-                    .then(resp=>{
-                        expanddiv.innerHTML += `<div class="expanddivcontent"><span class="yellow">last commit</span>: ${resp[0].commit.message} <span class="yellow">by</span> ${resp[0].commit.author.name}</div>`
-                    })
+                    
                 })
+                .then(
+                    fetch('https://api.github.com/repos/ctrl-team/' + repo.name + '/commits/master')
+                    .then(resp1 => resp1.json())
+                    .then(resp1=>{
+
+                        expanddiv.innerHTML += `<div class="expanddivcontent"><span class="yellow">last commit</span>: ${resp1.commit.message} <span class="yellow">by</span> ${resp1.author.login}</div>`
+                }))
 
 
                 reposlist.appendChild(repowrap)
