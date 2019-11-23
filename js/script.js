8ae570be60192cbe95b24cd59de89275c6397e60
Nimplex <39964594+Nimplex@users.noreply.github.com>
2019-11-23 21:44:04 +0100

    Rewrite sekcji info & Automatycznie generowane projekty , games

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 const logo_section = document.getElementById('logo');
 const backtotop = document.getElementById('backtotop');
-
 let logo_height = logo_section.offsetHeight;
-
 window.addEventListener('scroll', ()=>{
     if(window.scrollY > logo_height){
         backtotop.style.display = 'block';
@@ -12,6 +10,88 @@ window.addEventListener('scroll', ()=>{
         backtotop.style.opacity = 0;
     }
 })
+
+let games = [
+    {
+        "url": "https://noobclicker.glitch.me/",
+        "name": "NoobClicker",
+        "screenshot": "noob.png"
+    },
+    {
+        "url": "https://jebleb.glitch.me/",
+        "name": "Jebleb",
+        "screenshot": "jebleb.png"
+    },
+    {
+        "url": "https://keyguess.glitch.me/",
+        "name": "KeyGuess",
+        "screenshot": "keyguess.png"
+    },
+    {
+        "url": "https://wordguess.netlify.com/",
+        "name": "WordGuess",
+        "screenshot": "wordguess.png"
+    },
+    {
+        "url": "https://spooky-game.glitch.me/",
+        "name": "Spooky Game",
+        "screenshot": "SpookyGame.png"
+    },
+]
+let project = [
+    {
+        "url": "https://github.com/ctrl-team/hcjtemplate",
+        "name": "Hcjtemplate",
+        "screenshot": "hcjtemplate.png"
+    },
+    {
+        "url": "https://github.com/ctrl-team/CPP-Autoclicker/",
+        "name": "CPP autoclicker",
+        "screenshot": "autoclicker.png"
+    },
+    {
+        "url": "https://github.com/ctrl-team/BatPaint/",
+        "name": "BatPaint",
+        "screenshot": "batpiant.png"
+    },
+    {
+        "url": "https://spadaczka.glitch.me/",
+        "name": "Spadaczka",
+        "screenshot": "spadaczka.png"
+    },
+    {
+        "url": "https://zygurek.glitch.me/",
+        "name": "Zygurek",
+        "screenshot": "zygurek.png"
+    },
+    {
+        "url": "https://plikojad.glitch.me/",
+        "name": "Plikojad",
+        "screenshot": "plikojad.png"
+    },
+]
+for (let i = 0; i < games.length; i++) {
+    let game = document.createElement("div")
+    game.className = "game";
+    game.innerHTML = `
+          <a target="_blank" href="${games[i]["url"]}">
+            <img src="img/${games[i]["screenshot"]}" height="258"></img>
+          </a>
+          <p><a target="_blank" href="${games[i]["url"]}">${games[i]["name"]}</a></p>
+          `
+    document.getElementById("games").appendChild(game)
+}
+for (let i = 0; i < project.length; i++) {
+    let projects = document.createElement("div")
+    projects.className = "project";
+    projects.innerHTML = `
+          <a target="_blank" href="${project[i]["url"]}">
+            <img src="img/${project[i]["screenshot"]}" height="258"></img>
+          </a>
+          <p><a target="_blank" href="${project[i]["url"]}">${project[i]["name"]}</a></p>
+          `
+    document.getElementById("projects").appendChild(projects)
+}
 
 backtotop.addEventListener('click', ()=>{
     window.scrollTo(0, 0)
